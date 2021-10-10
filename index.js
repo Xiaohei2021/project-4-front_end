273424e20e2f7dab3773a65150bd5c4d342a2e80
Leon <hualiang.tian@gmail.com>
2021-10-10 16:39:01 -0400

    adjusting pokemon form and setting up  an event listener function

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const source = `http://localhost:3000`;
 const pokemonCall = new PokemonService(source);
+const pokemonForm = document.getElementById("pokemon-builder")
+const teamSelect = document.getElementById("team-dropdown")
+
 
 pokemonCall.getPokemons()
+
+pokemonForm.addEventListener("submit", handlesubmit)
 
