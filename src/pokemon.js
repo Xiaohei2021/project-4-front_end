f56c19c069fb3aff472187d28ca6dd4605903d94
Leon <hualiang.tian@gmail.com>
2021-10-09 22:11:45 -0400

    instantiated pokemon class and added constructor method

--- src/pokemon.js
+++ src/pokemon.js
@@ -1,1 +1,15 @@
- 
+ class Pokemon
+ {
+    constructor(name, pokemon_type, moves, abilities, national_pokedex_num, team_id, team)
+    {
+        this.name = name
+        this.pokemon_type = pokemon_type
+        this.moves = moves
+        this.abilities = abilities
+        this.national_pokedex_num = national_pokedex_num 
+        this.team_id = team_id
+        this.team = team
+    }
+
+    
+ }
