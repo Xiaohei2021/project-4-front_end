6ecd735b7c62a628ebfbf6647f85faa53db733da
Leon <hualiang.tian@gmail.com>
2021-10-11 20:04:22 -0400

    added team builder button and event handler

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+let buildTeam = true;
+
 const source = `http://localhost:3000`;
 const pokemonCall = new PokemonService(source);
 const teamCall = new TeamService(source);
+
+const buildbtn = document.getElementById("team-creator");
+const formbBlock = document.getElementById("registration-block")
 const pokemonForm = document.getElementById("pokemon-builder");
 const teamSelect = document.getElementById("team-dropdown");
 
@@ -13,11 +18,26 @@ const monDexNum = document.getElementById("pokemon-national_pokedex_num");
 pokemonCall.getPokemons();
 teamCall.getTeams();
 
-pokemonForm.addEventListener("submit", handleSubmit)
+pokemonForm.addEventListener("submit", handleSubmit);
 
 function handleSubmit(e)
 {
     e.preventDefault();
     // debugger
     pokemonCall.createPokemons();
+}
+
+buildbtn.addEventListener("click", handleBuild);
+
+function handleBuild(e)
+{
+    buildTeam =! buildTeam;
+    if (buildTeam)
+    {
+        formbBlock.style.display = "block";         
+    }
+    else
+    {
+        formbBlock.style.display = "none";
+    }
 }
